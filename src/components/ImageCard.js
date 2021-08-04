c01d23de33afabbc6c347f4e63b8ed2d71c48c8a
Cody <hoban.j.cody@gmail.com>
2021-08-04 18:22:51 -0400

    card content to material-ui must correct spans

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 class ImageCard extends React.Component {
   constructor(props) {
@@ -10,27 +17,49 @@ class ImageCard extends React.Component {
   }
 
   componentDidMount() {
+    console.log(this.props);
     this.imageRef.current.addEventListener('load', this.setSpans);
   }
 
   setSpans = () => {
     const height = this.imageRef.current.clientHeight;
 
-    const spans = Math.ceil(height / 10);
+    const spans = Math.ceil(height / 5);
 
     this.setState({ spans });
   };
 
   render() {
-    const { description, urls } = this.props.image;
+    const { description, urls, tags, alt_description } = this.props.image;
     return (
-      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
-        <img 
-          ref={this.imageRef}
-          alt={description} 
-          src={urls.regular} 
-        />
-      </div>
+      <Card style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <CardActionArea>
+          <CardMedia>
+            <img 
+              ref={this.imageRef}
+              alt={description} 
+              src={urls.regular}
+              title={tags[1]}
+            />
+          </CardMedia>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {tags[1].title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {alt_description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
     );
   }
 }
